1bfbc9914277a79d84c0730e1b129c869641646d
thehaut <theo.luret@e-artsup.net>
2022-12-12 15:18:49 +0100

    Chrono + placeholder UI

---
+++ Assets/Scripts/Chrono.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Chrono : MonoBehaviour
+{
+    [SerializeField] private Image uiFill;
+
+    public int Duration;
+    private int remainingDuration;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Being(Duration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Being (int Second)
+    {
+        remainingDuration = Second;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while (remainingDuration >= 0)
+        {
+            uiFill.fillAmount = Mathf.InverseLerp(0, Duration, remainingDuration);
+            remainingDuration--;
+            yield return new WaitForSeconds(1f);
+        }
+    }
+}
