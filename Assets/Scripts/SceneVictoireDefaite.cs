b0beec030f424a3084b2ce45f8626e49f98ff3f9
LavaudAlexandre <104425624+LavaudAlexandre@users.noreply.github.com>
2022-12-16 10:07:16 +0100

    scene VictoryDefeat

---
+++ Assets/Scripts/SceneVictoireDefaite.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneVictoireDefaite : MonoBehaviour
+{
+    
+    public GameObject win;
+    public GameObject lose;
+
+    AudioSource Test;
+
+    private void Update()
+    {
+        
+    }
+
+    public void PV()
+    {
+                
+    }
+
+
+
+
+
+
+}
